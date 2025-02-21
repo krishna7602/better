85c9fa8a3551b287a030813b4ec2274198728698
ramkrishna mondal <ramkrishnam170@gmail.com>
2025-02-21 14:33:31 +0530

    Update link in Start component to redirect to home page

--- src/pages/Start.jsx
+++ src/pages/Start.jsx
@@ -10,7 +10,7 @@ const Start = () => {
       <div className='bg-[#004733] min-h-screen flex flex-col items-center justify-center text-white'>
         <h1 className='text-4xl md:text-6xl font-bold mb-8'>Start Your Journey</h1>
         <p className='text-lg md:text-xl mb-8'>Get pre-approved for a mortgage in minutes.</p>
-        <Link to='/home' className='px-6 py-3 bg-[#1EE07F] text-black text-sm rounded-full hover:bg-[#004733]'>Get Started</Link>
+        <Link to='/' className='px-6 py-3 bg-[#1EE07F] text-black text-sm rounded-full hover:bg-[#004733]'>Get Started</Link>
       </div>
       <Footer />
     </>
